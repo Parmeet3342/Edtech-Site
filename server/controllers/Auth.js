639f76422b5b0349f9b463517e0f8fec1a25c855
Parmeet Singh <126928021+Parmeet3342@users.noreply.github.com>
2026-04-18 14:03:59 +0530

    Send verification email with OTP

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -52,6 +52,11 @@ exports.sendOTP = async(req,res) => {
             otp
         })
 
+		await mailSender(
+            email,
+            "Verification Email",
+            otpTemplate(otp)
+       );
         return res.status(200).json({
             success:true,
             message:"OTP sent successfully",
